b69f3da6a1d464464ad12aa029bd0ef025a99b96
root <root@dns.suppi147.local>
2023-03-13 21:27:08 +0700

    add space and empty string check

--- bai-tap-login-form/backend/shop-cute-main/check.js
+++ bai-tap-login-form/backend/shop-cute-main/check.js
@@ -1,13 +1,15 @@
 function check()
 {
-    var n1 = document.getElementById("username");
-    var n2 = document.getElementById("password");
-    if(n1 != " " && n2 != " ")
+    var n1 = document.getElementById("username").value;
+    var n2 = document.getElementById("password").value;
+
+    if(n1 === "" || n2 === ""||((/\s/).test(n1)==true || (/\s/).test(n2)==true))
     {
-        return true;
+        alert("Khong duoc de trong");
+	return false;
     }
     else 
     {
-        alert("Khong duoc de trong");
+        return true;
     }
 }
